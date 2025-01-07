c0ee9425dcba92b0874c26e655ee1c1c76a10295
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2025-01-07 14:30:55 +0530

    captain service done

--- gateway/app.js
+++ gateway/app.js
@@ -6,6 +6,8 @@ const app = express()
 
 
 app.use('/user', expressProxy('http://localhost:3001'))
+app.use('/captain', expressProxy('http://localhost:3002'))
+
 
 app.listen(3000, () => {
     console.log('Gateway server listening on port 3000')
