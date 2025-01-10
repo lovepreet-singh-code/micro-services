60885076a2d03324505ff24bf59b72bb2d5bee76
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2025-01-10 14:09:02 +0530

    ride services done

--- user/app.js
+++ user/app.js
@@ -6,6 +6,10 @@ const connect = require('./db/db')
 connect()
 const userRoutes = require('./routes/user.routes')
 const cookieParser = require('cookie-parser')
+const rabbitMq = require('./service/rabbit')
+
+rabbitMq.connect()
+
 
 
 
