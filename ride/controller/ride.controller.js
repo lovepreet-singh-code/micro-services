606ee2aa111406e77bd0b483d2426381c0962abd
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2025-01-12 18:05:33 +0530

    update ride service

--- ride/controller/ride.controller.js
+++ ride/controller/ride.controller.js
@@ -24,15 +24,15 @@ module.exports.createRide = async (req, res, next) => {
 
 }
 
-// module.exports.acceptRide = async (req, res, next) => {
-//     const { rideId } = req.query;
-//     const ride = await rideModel.findById(rideId);
-//     if (!ride) {
-//         return res.status(404).json({ message: 'Ride not found' });
-//     }
+module.exports.acceptRide = async (req, res, next) => {
+    const { rideId } = req.query;
+    const ride = await rideModel.findById(rideId);
+    if (!ride) {
+        return res.status(404).json({ message: 'Ride not found' });
+    }
 
-//     ride.status = 'accepted';
-//     await ride.save();
-//     publishToQueue("ride-accepted", JSON.stringify(ride))
-//     res.send(ride);
-// }
+    ride.status = 'accepted';
+    await ride.save();
+    publishToQueue("ride-accepted", JSON.stringify(ride))
+    res.send(ride);
+}
